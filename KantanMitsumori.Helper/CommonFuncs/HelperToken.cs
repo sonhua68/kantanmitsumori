280ca9170d2199cda1810f2c25875476bf45a3f8
HoaiPhong <phong.le@autoserver.vn>
2022-08-02 10:25:15 +0700

    Clean code

--- KantanMitsumori.Helper/CommonFuncs/HelperToken.cs
+++ KantanMitsumori.Helper/CommonFuncs/HelperToken.cs
@@ -15,22 +15,18 @@ namespace KantanMitsumori.Helper.CommonFuncs
 {
     public static class HelperToken
     {
-        public static IConfiguration _config;
+        public static IConfiguration? _config;
         public static void Configure(IConfiguration config)
         {
             _config = config;
-
-
         }
-
-        public static LogToken EncodingToken(string token)
+        public static LogToken? EncodingToken(string token)
         {
             try
             {
                 if (string.IsNullOrWhiteSpace(token)) return null;
-                var tokenHandler = new JwtSecurityTokenHandler();
-                if (_config == null) return null;
-                var key = Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]);
+                var tokenHandler = new JwtSecurityTokenHandler();            
+                var key = Encoding.UTF8.GetBytes(_config!["JwtSettings:Key"]);
                 tokenHandler.ValidateToken(token, new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
@@ -44,22 +40,18 @@ namespace KantanMitsumori.Helper.CommonFuncs
                 var genderStr = jwtToken.Claims.First(x => x.Type == JwtRegisteredClaimNames.Sub).Value;
                 if (!string.IsNullOrWhiteSpace(genderStr))
                 {
-                    LogToken model = JsonConvert.DeserializeObject<LogToken>(genderStr);
+                    var model = JsonConvert.DeserializeObject<LogToken>(genderStr);
                     return model;
                 }
-
                 return null;
             }
-            catch (Exception ex)
+            catch 
             {
                 return null;
             }
-
-
         }
         public static string GenerateJsonToken(LogToken model)
-        {
-            var jwtKey = _config["JwtSettings:Key"];
+        {           
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
             string genderStr = JsonConvert.SerializeObject(model);
@@ -71,7 +63,6 @@ namespace KantanMitsumori.Helper.CommonFuncs
             var currentDate = DateTime.Now;
             var RefreshExpires = _config["JwtSettings:RefreshExpires"];
             TimeSpan time = TimeSpan.Parse(RefreshExpires);
-
             var token = new JwtSecurityToken(
                 issuer: _config["JwtSettings:Issuer"],
                 audience: _config["JwtSettings:Issuer"],
