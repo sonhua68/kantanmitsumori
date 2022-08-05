25bc7da6ecd57ccea17c29d5599657cb22e8046e
Huy Pham <huy.pham@autoserver.vn>
2022-08-05 15:25:22 +0700

    update comon UI

--- KantanMitsumori/Startup.cs
+++ KantanMitsumori/Startup.cs
@@ -69,7 +69,7 @@ namespace KantanMitsumori
                 endpoints.MapControllers();
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Test}/{id?}"
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
                 );
             });
         }
