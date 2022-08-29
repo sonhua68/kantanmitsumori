810938fd312af52525c4c600653de3ab5e20c6c0
HoaiPhong <phong.le@autoserver.vn>
2022-08-29 09:54:28 +0700

    update code

--- KantanMitsumori/Controllers/HomeController.cs
+++ KantanMitsumori/Controllers/HomeController.cs
@@ -71,7 +71,7 @@ namespace KantanMitsumori.Controllers
         public IActionResult Inpcar()
         {            
             RequestInputCar res = new RequestInputCar();
-            res.EstNo = "22082300011";
+            res.EstNo = "22082900004";
             res.EstSubNo = "01";
             var response = _estimateService.GetDetail(res);
             return View(response.Data);
@@ -79,7 +79,7 @@ namespace KantanMitsumori.Controllers
         public IActionResult InpHanbaiten()
         {
             RequestInputCar res = new RequestInputCar();
-            res.EstNo = "22082300011";
+            res.EstNo = "22082900004";
             res.EstSubNo = "01";
             var response = _estimateService.GetDetail(res);
             return View(response.Data);
@@ -87,7 +87,7 @@ namespace KantanMitsumori.Controllers
         public IActionResult InpLoan()
         {
             RequestInputCar res = new RequestInputCar();
-            res.EstNo = "22082300011";
+            res.EstNo = "22082900004";
             res.EstSubNo = "01";
             var response = _estimateService.GetDetail(res);
             return View(response.Data);
---
+++ KantanMitsumori/Controllers/InpLoanController.cs
@@ -0,0 +1,48 @@
+﻿using KantanMitsumori.Helper.CommonFuncs;
+using KantanMitsumori.Helper.Enum;
+using KantanMitsumori.IService;
+using KantanMitsumori.Model;
+using KantanMitsumori.Model.Request;
+using KantanMitsumori.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KantanMitsumori.Controllers
+{
+    public class InpLoanController : BaseController
+    {
+        private readonly IAppService _appService;
+        private readonly IEstimateService _estimateService;
+        private readonly ILogger<InpLoanController> _logger;
+        public InpLoanController(IAppService appService, IEstimateService estimateService,  ILogger<InpLoanController> logger)
+        {
+            _appService = appService;
+            _estimateService = estimateService;
+            _logger = logger;
+        }    
+
+      
+        #region HoaiPhong
+   
+     
+        public IActionResult InpLoan()
+        {
+            RequestInputCar res = new RequestInputCar();
+            res.EstNo = "22082900004";
+            res.EstSubNo = "01";
+            var response = _estimateService.GetDetail(res);
+            return View(response.Data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateInpLoan([FromForm] RequestUpdateInputCar requestData)
+        {
+            var response = await _estimateService.UpdateInputCar(requestData);
+            if (response.ResultStatus == (int)enResponse.isError)
+            {
+                return ErrorAction(response);
+            }
+            return Ok(response);
+        }      
+        #endregion HoaiPhong
+    }
+}
