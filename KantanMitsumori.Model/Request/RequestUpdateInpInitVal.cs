018b1d5d44624577ed1f87501c84d221ef06ba49
HoaiPhong <phong.le@autoserver.vn>
2022-09-16 23:54:55 +0700

    update model request

--- KantanMitsumori.Model/Request/RequestUpdateInpInitVal.cs
+++ KantanMitsumori.Model/Request/RequestUpdateInpInitVal.cs
@@ -17,117 +17,57 @@ namespace KantanMitsumori.Model.Request
         public string? ShopAdr { get; set; }
         public string? ShopTel { get; set; }
         public string? EstTanName { get; set; }
-        public int? YtiRiekiH { get; set; }
-        public int? YtiRiekiK { get; set; }
-        public int? SyakenNewH { get; set; }
-        public int? SyakenNewK { get; set; }
-        public int? SyakenZokH { get; set; }
-        public int? SyakenZokK { get; set; }
-        public int? TaxFreeCheckH { get; set; }
-        public int? TaxFreeCheckK { get; set; }
-        public int? TaxFreeGarageH { get; set; }
-        public int? TaxFreeGarageK { get; set; }
-        public int? TaxFreeTradeInH { get; set; }
-        public int? TaxFreeTradeInK { get; set; }
-        public int? TaxCheckH { get; set; }
-        public int? TaxCheckK { get; set; }
-        public int? TaxGarageH { get; set; }
-        public int? TaxGarageK { get; set; }
-        public int? TaxTradeInH { get; set; }
-        public int? TaxTradeInK { get; set; }
-        public int? TaxTradeInChkH { get; set; }
-        public int? TaxTradeInChkK { get; set; }
-        public int? TaxRecycleH { get; set; }
-        public int? TaxRecycleK { get; set; }
-        public int? TaxDeliveryH { get; set; }
-        public int? TaxDeliveryK { get; set; }
+        public int YtiRiekiH { get; set; }
+        public int YtiRiekiK { get; set; }
+        public int SyakenNewH { get; set; }
+        public int SyakenNewK { get; set; }
+        public int SyakenZokH { get; set; }
+        public int SyakenZokK { get; set; }
+        public int TaxFreeCheckH { get; set; }
+        public int TaxFreeCheckK { get; set; }
+        public int TaxFreeGarageH { get; set; }
+        public int TaxFreeGarageK { get; set; }
+        public int TaxFreeTradeInH { get; set; }
+        public int TaxFreeTradeInK { get; set; }
+        public int TaxCheckH { get; set; }
+        public int TaxCheckK { get; set; }
+        public int TaxGarageH { get; set; }
+        public int TaxGarageK { get; set; }
+        public int TaxTradeInH { get; set; }
+        public int TaxTradeInK { get; set; }
+        public int TaxTradeInChkH { get; set; }
+        public int TaxTradeInChkK { get; set; }
+        public int TaxRecycleH { get; set; }
+        public int TaxRecycleK { get; set; }
+        public int TaxDeliveryH { get; set; }
+        public int TaxDeliveryK { get; set; }
         public string? TaxSet1Title { get; set; }
-        public int? TaxSet1H { get; set; }
-        public int? TaxSet1K { get; set; }
+        public int TaxSet1H { get; set; }
+        public int TaxSet1K { get; set; }
         public string? TaxSet2Title { get; set; }
-        public int? TaxSet2H { get; set; }
-        public int? TaxSet2K { get; set; }
+        public int TaxSet2H { get; set; }
+        public int TaxSet2K { get; set; }
         public string? TaxSet3Title { get; set; }
-        public int? TaxSet3H { get; set; }
-        public int? TaxSet3K { get; set; }
+        public int TaxSet3H { get; set; }
+        public int TaxSet3K { get; set; }
         public string? TaxFreeSet1Title { get; set; }
-        public int? TaxFreeSet1H { get; set; }
+        public int TaxFreeSet1H { get; set; }
         public string? TaxFreeSet1K { get; set; }
         public string? TaxFreeSet2Title { get; set; }
-        public int? TaxFreeSet2H { get; set; }
-        public int? TaxFreeSet2K { get; set; }
+        public int TaxFreeSet2H { get; set; }
+        public int TaxFreeSet2K { get; set; }
         public string? DamageInsMonth { get; set; }
-        public byte? AsArticle { get; set; }
+        public byte AsArticle { get; set; }
         public string? MemberUrl { get; set; }     
-        public bool? Dflag { get; set; }
+        public bool Dflag { get; set; }
         public string? SekininName { get; set; }
         public string? Jibai { get; set; }
         public string? Article { get; set; }
         public string? Szi { get; set; }    
         public string? ButtonSummit { get; set; }
-        public int? Haiki { get; set; }
+        public int Haiki { get; set; }
         public string? sesMode { get; set; }  
         public string? SyakenNewZok { get; set; }
-
-        //public decimal TaxRatio { get; set; }
-        //public string? hidConTaxInputKb { get; set; }
-        //public string? YtiRiekiH { get; set; }
-        //public string? YtiRiekiK { get; set; }
-        //public string? SyakenNewH { get; set; }
-        //public string? SyakenNewK { get; set; }
-        //public string? SyakenZokH { get; set; }
-        //public string? SyakenZokK { get; set; }
-        //public string? TaxFreeCheckH { get; set; }
-        //public string? TaxFreeCheckK { get; set; }
-        //public string? TaxFreeGarageH { get; set; }
-        //public string? TaxFreeGarageK { get; set; }
-        //public string? TaxFreeTradeInH { get; set; }
-        //public string? TaxFreeTradeInK { get; set; }
-        //public string? TaxFreeSet1Title { get; set; }
-        //public string? TaxFreeSet1H { get; set; }
-        //public string? TaxFreeSet1K { get; set; }
-        //public string? TaxFreeSet2Title { get; set; }
-        //public string? TaxFreeSet2H { get; set; }
-        //public string? TaxFreeSet2K { get; set; }
-        //public string? TaxCheckH { get; set; }
-        //public string? TaxCheckK { get; set; }
-        //public string? TaxGarageH { get; set; }
-        //public string? TaxGarageK { get; set; }
-        //public string? TaxTradeInH { get; set; }
-        //public string? TaxTradeInK { get; set; }
-        //public string? TaxTradeInChkH { get; set; }
-        //public string? TaxTradeInChkK { get; set; }
-        //public string? TaxRecycleH { get; set; }
-        //public string? TaxRecycleK { get; set; }
-        //public string? TaxDeliveryH { get; set; }
-        //public string? TaxDeliveryK { get; set; }
-        //public string? TaxSet1Title { get; set; }
-        //public string? TaxSet1H { get; set; }
-        //public string? TaxSet1K { get; set; }
-        //public string? TaxSet2Title { get; set; }
-        //public string? TaxSet2H { get; set; }
-        //public string? TaxSet2K { get; set; }
-        //public string? TaxSet3Title { get; set; }
-        //public string? TaxSet3H { get; set; }
-        //public string? TaxSet3K { get; set; }
-        //public string? Rate { get; set; }
-        //public string? ShopNm { get; set; }
-        //public string? ShopAdr { get; set; }
-        //public string? ShopTel { get; set; }
-        //public string? EstTanName { get; set; }
-        //public string? Sekinin { get; set; }
-        //public string? Article { get; set; }
-        //public string? MemberURL { get; set; }
-        //public string? TaxRatio { get; set; }
-        //public string? UserNo { get; set; }
-        //public string? EstNo { get; set; }
-        //public string? EstSubNo { get; set; }
-        //public string? Haiki { get; set; }
-        //public string? SyakenNewZok { get; set; }
-        //public string? btnOK { get; set; }
-        //public string? Jibai { get; set; }
-
-
     }
 }
 
