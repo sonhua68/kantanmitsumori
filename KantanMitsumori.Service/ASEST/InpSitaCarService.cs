bddc3342569e19a5a484e439f5aba8f391e85834
Huy Pham <huy.pham@autoserver.vn>
2022-09-22 13:47:27 +0700

    update set tax for input sita car page

--- KantanMitsumori.Service/ASEST/InpSitaCarService.cs
+++ KantanMitsumori.Service/ASEST/InpSitaCarService.cs
@@ -48,8 +48,15 @@ namespace KantanMitsumori.Service.ASEST
                 model = _mapper.Map<ResponseInpSitaCar>(estData);
 
                 // 預り法定費用、手続代行費用のセット
-                if (model.TradeInUM != 1 || !(model.TradeInUM == 0 &&
-                    (model.TaxFreeTradeIn > 0 || model.TaxTradeIn > 0 || model.TaxTradeInSatei > 0 || model.TradeInCarName != "")))
+                if (model.TradeInUM == 1 || (model.TradeInUM == 0 &&
+                    (model.TaxFreeTradeIn > 0 || model.TaxTradeIn > 0 || model.TaxTradeInSatei > 0 || !string.IsNullOrEmpty(model.TradeInCarName))))
+
+                {
+                    model.TaxFreeTradeIn = estData.TaxFreeTradeIn;
+                    model.TaxTradeIn = estData.TaxTradeIn;
+                    model.TaxTradeInSatei = estData.TaxTradeInSatei;
+                }
+                else
                 {
                     // 隠しフィールドには、設定レコードがあればその値を反映
                     var getUserDef = _commonFuncHelper.getUserDefData(userNo);
