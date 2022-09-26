6f48da84fca0e3ac9180644116a15872ff9b0ad1
dang.pham <dang.pham@autoserver.vn>
2022-09-26 13:16:25 +0700

    Add InpCarPrice view and controller

---
+++ KantanMitsumori/Controllers/InpCarPriceController.cs
@@ -0,0 +1,46 @@
+﻿using KantanMitsumori.Helper.Enum;
+using KantanMitsumori.IService;
+using KantanMitsumori.IService.ASEST;
+using KantanMitsumori.Model.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KantanMitsumori.Controllers
+{
+    public class InpCarPriceController : BaseController
+    {
+        private readonly IEstMainService _appService;
+        private readonly IEstimateService _estimateService;
+        private readonly ILogger<InpHanbaitenController> _logger;
+        public InpCarPriceController(IEstMainService appService, IConfiguration config, IEstimateService estimateService, ILogger<InpHanbaitenController> logger) : base(config)
+        {
+            _appService = appService;
+            _estimateService = estimateService;
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            RequestInp request = new RequestInp();
+            request.EstNo = _logToken.sesEstNo;
+            request.EstSubNo = _logToken.sesEstSubNo;
+            request.UserNo = _logToken.UserNo;
+            request.TaxRatio = _logToken.sesTaxRatio;
+            var response = _estimateService.GetDetail(request);
+            if (response.ResultStatus == (int)enResponse.isError)
+            {
+                return ErrorAction(response);
+            }
+            return View(response.Data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateInpHanbaiten([FromForm] RequestUpdateInpHanbaiten requestData)
+        {
+            var response = await _estimateService.UpdateInpHanbaiten(requestData);
+            if (response.ResultStatus == (int)enResponse.isError)
+            {
+                return ErrorAction(response);
+            }
+            return Ok(response);
+        }        
+    }
+}
