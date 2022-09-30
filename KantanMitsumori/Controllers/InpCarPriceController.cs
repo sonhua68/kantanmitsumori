0f96406ff19a7efb5bac651e702864bc5cd96186
dang.pham <dang.pham@autoserver.vn>
2022-09-30 17:44:19 +0700

    Add InpCarPrice controller and view

--- KantanMitsumori/Controllers/InpCarPriceController.cs
+++ KantanMitsumori/Controllers/InpCarPriceController.cs
@@ -1,6 +1,8 @@
-﻿using KantanMitsumori.Helper.Enum;
+﻿using AutoMapper;
+using KantanMitsumori.Helper.Enum;
 using KantanMitsumori.IService;
 using KantanMitsumori.IService.ASEST;
+using KantanMitsumori.Model;
 using KantanMitsumori.Model.Request;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,39 +10,61 @@ namespace KantanMitsumori.Controllers
 {
     public class InpCarPriceController : BaseController
     {
-        private readonly IEstMainService _appService;
-        private readonly IEstimateService _estimateService;
-        private readonly ILogger<InpHanbaitenController> _logger;
-        public InpCarPriceController(IEstMainService appService, IConfiguration config, IEstimateService estimateService, ILogger<InpHanbaitenController> logger) : base(config)
+        private readonly IInpCarPriceService _inpCarPriceService;        
+        private readonly ILogger<InpCarPriceController> _logger;
+        private readonly IMapper _mapper;
+        public InpCarPriceController(IConfiguration config
+            , ILogger<InpCarPriceController> logger
+            , IMapper mapper
+            , IInpCarPriceService inpCarPriceService
+            ) : base(config)
         {
-            _appService = appService;
-            _estimateService = estimateService;
+            _inpCarPriceService = inpCarPriceService;
             _logger = logger;
+            _mapper = mapper;
         }
         
         public IActionResult Index()
         {
-            RequestInp request = new RequestInp();
-            request.EstNo = _logToken.sesEstNo;
-            request.EstSubNo = _logToken.sesEstSubNo;
-            request.UserNo = _logToken.UserNo;
-            request.TaxRatio = _logToken.sesTaxRatio;
-            var response = _estimateService.GetDetail(request);
+            // Create request model map values from logToken
+            RequestInpCarPrice request = new RequestInpCarPrice();
+            _mapper.Map(_logToken, request);
+            // Get car price data
+            var response = _inpCarPriceService.GetCarPriceInfo(request);
             if (response.ResultStatus == (int)enResponse.isError)
             {
                 return ErrorAction(response);
             }
-            return View(response.Data);
+            // Show view
+            return View("Index", response.Data);
         }
+
         [HttpPost]
-        public async Task<IActionResult> UpdateInpHanbaiten([FromForm] RequestUpdateInpHanbaiten requestData)
+        public async Task<IActionResult> Update(RequestUpdateInpCarPrice requestData)
         {
-            var response = await _estimateService.UpdateInpHanbaiten(requestData);
-            if (response.ResultStatus == (int)enResponse.isError)
+            // Create model for update car price
+            var model = new RequestUpdateCarPrice();
+            _mapper.Map(requestData, model);            
+            // Get car price data
+            var response =   _inpCarPriceService.Update(model);            
+            return Json(response);
+        }
+
+        public IActionResult Demo()
+        {
+            CreateLogTokenForDemo();
+            return Index();
+        }
+
+        private void CreateLogTokenForDemo()
+        {
+            _logToken = new LogToken()
             {
-                return ErrorAction(response);
-            }
-            return Ok(response);
-        }        
+                sesEstNo = "22092900010",
+                sesEstSubNo = "01",
+                UserNo = "88888195",
+                UserNm = "DANGPHAM"                
+            };
+        }
     }
 }
--- KantanMitsumori/wwwroot/js/InpCarPrice.js
+++ KantanMitsumori/wwwroot/js/InpCarPrice.js
@@ -1,57 +1,11 @@
-﻿// JScript File
-
-//（iPad 対応以降不使用のため、コメントアウト）
-///**********************************************
-///* 車両価格DB更新
-///*********************************************/
-//function upCarPrice(){
-//
-//	//入力チェック
-//	if (inputChk() == false) {return;} 
-//
-//    //画面から値の取得
-//    
-//	var estno = $get('hidEstNo').value;
-//	var estsubno = $get('hidEstSubNo').value;
-//	
-//	var Price = $get('txtPrice').value;
-//	var RakuSatu = $get('txtRakuSatu').value;
-//	var Rikusou = $get('txtRikusou').value;
-//	var SonotaTitle = $get('txtSonotaTitle').value;
-//	var Sonota = $get('txtSonota').value;
-//	
-//	var SyakenSeibi = $get('txtSyakenSeibi').value;
-//	var CarSum = $get('txtCarSum').value;
-//	
-//	
-//	//車検費用か納車費用か
-//	var SyakenNewZok = $get('radSyakenY').checked;
-//	
-//	var carprice = new Array();
-//	carprice[0] = Price;
-//	carprice[1] = RakuSatu;
-//	carprice[2] = Rikusou;
-//	carprice[3] = SyakenSeibi;
-//	carprice[4] = SyakenNewZok;
-//	
-//	carprice[5] = SonotaTitle;
-//	carprice[6] = Sonota;
-//	carprice[7] = CarSum;
-//	
-//    asest2.WebService.updateCarPrice(estno,estsubno,carprice)
-//    window.returnValue=carprice;
-//    window.close();
-//}
-
-
-//入力チェック
+﻿var frm = document.getElementById('formInpCarPrice');
 function inputChk() {
 
     //2012-03-31 エラーメッセージ出力をHiddenからLabalに変更
 	document.getElementById("txMsg").innerHTML = "";
 	var outMsg;
 	
-	outMsg = chkKingaku("車両本体",$get('txtPrice').value);
+	outMsg = chkKingaku("車両本体",$get('txtCarPrice').value);
     //2012-03-31 エラーメッセージ出力をHiddenからLabalに変更
 	if (outMsg != "") {document.getElementById("txMsg").innerHTML = outMsg; return false;}
 	
@@ -89,7 +43,7 @@ function EstSum() {
 	//入力チェック
 	if (inputChk() == false) {return;} 
 
-	var Price = chkNull($get('txtPrice').value);
+	var Price = chkNull($get('txtCarPrice').value);
 	var RakuSatu = chkNull($get('txtRakuSatu').value);
 	var Rikusou = chkNull($get('txtRikusou').value);
 	
@@ -102,27 +56,18 @@ function EstSum() {
 }
 //整備費用のボタン切り替え時の金額取得
 function chgSeibi() {
-
-	var SyakenNewZok = $get('radSyakenY').checked;
-	var UserNo = $get('hidUserNo').value;
-	var DispVol = $get('hidDispVol').value;
-	
-	//整備費用呼出
-	var retSeibi = asest2.WebService.getSeibiPrice(UserNo,SyakenNewZok,DispVol,setSeibi)
-
+	var isSyakenZok = $get('radSyakenY').checked;
+	var userSyakenZok = $get('hidUserSyakenZok').value;
+	var userSyakenNew = $get('hidUserSyakenNew').value;
+	if(isSyakenZok)
+		setSeibi(userSyakenZok);
+	else
+		setSeibi(userSyakenNew);
 }
+
 function setSeibi(retSeibi) {
     if (retSeibi != 0) {
         frm.txtSyakenSeibi.value = retSeibi;
     }
     EstSum();
-}
-//初期表示時のラジオボタン制御
-function setSyakenNewZok() {
-	var SyakenNewZok = frm.hidSyakenNewZok.value;
-	if (SyakenNewZok == "Syaken") {
-        frm.radSyakenY.checked = true;
-    } else {
-        frm.radSyakenN.checked = true;
-    }
 }
