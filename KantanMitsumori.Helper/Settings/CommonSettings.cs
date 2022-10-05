88fa01e7ffb46437638ac2c1e42b47e7b6083bb0
dang.pham <dang.pham@autoserver.vn>
2022-10-05 10:38:46 +0700

    fixbug #2493 Split settings

---
+++ KantanMitsumori.Helper/Settings/CommonSettings.cs
@@ -0,0 +1,18 @@
+﻿using KantanMitsumori.Helper.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace KantanMitsumori.Helper.Settings
+{
+    public static class CommonSettings
+    {
+
+        public static IConfiguration? Configuration;
+        public static void Configure(IConfiguration config) => Configuration = config;        
+        public static TestSettings TestSettings => new TestSettings(Configuration!);
+        public static DataSettings DataSettings => new DataSettings(Configuration!);
+        public static PhysicalPathSettings PhysicalPathSettings => new PhysicalPathSettings(Configuration!);
+        public static URLSettings URLSettings => new URLSettings(Configuration!);
+        public static ConnectionStrings ConnectionStrings => new ConnectionStrings(Configuration!);
+        public static JwtSettings JwtSettings => new JwtSettings(Configuration!);
+    }
+}
