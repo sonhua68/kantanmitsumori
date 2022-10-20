7c0e852a557e2a3aefcc02d0a5b90c5b6d8cc926
Huy Pham <huy.pham@autoservn.vn>
2022-10-20 16:35:42 +0900

    #2586 Logo not display

--- KantanMitsumori.Service/ReportService.cs
+++ KantanMitsumori.Service/ReportService.cs
@@ -28,8 +28,9 @@ namespace KantanMitsumori.Service
         private readonly IUnitOfWorkIDE _unitOfWorkIDE;
         private readonly HelperMapper _helperMapper;
         private readonly PhysicalPathSettings _physicalPathSettings;
+        private readonly URLSettings _urlSettings;
 
-        public ReportService(IMapper mapper, ILogger<IReportService> logger, IUnitOfWork unitOfWork, IUnitOfWorkIDE unitOfWorkIDE, HelperMapper helperMapper, IOptions<PhysicalPathSettings> physicalPathSettings)
+        public ReportService(IMapper mapper, ILogger<IReportService> logger, IUnitOfWork unitOfWork, IUnitOfWorkIDE unitOfWorkIDE, HelperMapper helperMapper, IOptions<PhysicalPathSettings> physicalPathSettings, IOptions<URLSettings> urlSettings)
         {
             _mapper = mapper;
             _logger = logger;
@@ -37,6 +38,7 @@ namespace KantanMitsumori.Service
             _unitOfWorkIDE = unitOfWorkIDE;
             _helperMapper = helperMapper;
             _physicalPathSettings = physicalPathSettings.Value;
+            _urlSettings = urlSettings.Value;
         }
 
         public ResponseBase<ReportFileModel> GenerateReport(RequestReport model)
@@ -151,6 +153,9 @@ namespace KantanMitsumori.Service
         {
             UpdateSyakenNewZok(reportModel, estEntity, estSubEntity);
             UpdateUserDefInfo(reportModel, userEntity);
+
+            // set logo
+            reportModel.AutoFlatLogo = _urlSettings.AutoFlagLogoUrl;
         }
 
         private void UpdateUserDefInfo(EstimateReportModel reportModel, MUserDef userEntity)
