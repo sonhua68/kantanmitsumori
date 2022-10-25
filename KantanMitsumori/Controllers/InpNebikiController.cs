9c9cfcfd1e90d7aaa1d6bc19f774a73e37361491
HoaiPhong <phong.le@autoserver.vn>
2022-10-25 14:25:41 +0700

    fix bug #2633 値引き (Discount) isn't applied to total price

--- KantanMitsumori/Controllers/InpNebikiController.cs
+++ KantanMitsumori/Controllers/InpNebikiController.cs
@@ -33,7 +33,7 @@ namespace KantanMitsumori.Controllers
         [HttpPost]
         public async Task<IActionResult> UpdateInpNebiki([FromForm] RequestUpdateInpNebiki requestData)
         {
-            var response = await _estimateService.UpdateInpNebiki(requestData);
+            var response = await _estimateService.UpdateInpNebiki(requestData,_logToken!);
             return Ok(response);
         }
         #endregion InpNebiki
