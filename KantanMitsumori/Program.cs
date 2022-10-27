47949b2cbd0ae9bb2e38597c2ed5f075b54b185f
HoaiPhong <phong.le@autoserver.vn>
2022-10-27 16:13:40 +0700

    setting  nlog

--- KantanMitsumori/Program.cs
+++ KantanMitsumori/Program.cs
@@ -1,10 +1,26 @@
 using KantanMitsumori;
+using NLog.Web;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+        try
+        {
+            logger.Debug("init main function");
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "Error in init");
+            throw;
+        }
+        finally
+        {
+            NLog.LogManager.Shutdown();
+        }
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -13,6 +29,11 @@ public class Program
             {
                 webBuilder.UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Information);
+                })
+            .UseNLog();
             });
 }
